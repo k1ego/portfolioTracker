b23c35b2fc8fc11822d3c8021453c67334a27193
klego <ladonin05@mail.ru>
2025-09-08 00:09:40 +0300

    Реализация React Context

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,45 +1,15 @@
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
-import { Card, Layout, List, Spin, Statistic, Tag, Typography } from 'antd'
-import { useEffect, useState } from 'react'
-import { fakeFetchCrypto, fetchAssets } from '../../api'
-import { capitalize, percentDifference } from '../../utils'
+import { Card, Layout, List, Statistic, Tag, Typography } from 'antd'
+import { useContext } from 'react'
+import CryptoContext from '../../context/crypto-context'
+import { capitalize } from '../../utils'
 
 const siderStyle = {
 	padding: '1rem',
 }
 
 export default function AppSider() {
-	const [loading, setLoading] = useState(false)
-	const [crypto, setCrypto] = useState([])
-	const [assets, setAssets] = useState([])
-
-	useEffect(() => {
-		async function preload() {
-			setLoading(true)
-			const { result } = await fakeFetchCrypto()
-			const assets = await fetchAssets()
-
-			setAssets(
-				assets.map(asset => {
-					const coin = result.find(c => c.id === asset.id)
-					return {
-						grow: asset.price < coin.price,
-						growPercent: percentDifference(asset.price, coin.price),
-						totalAmount: asset.amount * coin.price,
-						totalProfit: asset.amount * coin.price - asset.amount * asset.price,
-						...asset,
-					}
-				})
-			)
-			setCrypto(result)
-			setLoading(false)
-		}
-		preload()
-	}, [])
-
-	if (loading) {
-		return <Spin fullscreen />
-	}
+	const { loading, assets } = useContext(CryptoContext)
 
 	return (
 		<Layout.Sider width='25%' style={siderStyle}>
